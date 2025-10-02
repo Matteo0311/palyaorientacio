2345ca1478c3cdd92459458786bccc454080fb78
Matteo <levaimate03@gmail.com>
2025-10-02 14:08:34 +0200

    Update App component description for career orientation guidance

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
   return (
     <div className="App">
       <h1>Pályaorientáció</h1>
-      <p>Tesztelek</p>
+      <p>Ha pályaváltás, vagyis új, az eddigitől eltérő foglalkozás választása előtt áll, érdemes
+tisztáznia, hogy milyen képességekkel rendelkezik. A képességek erősen befolyásolják, hogy
+pályáján, foglalkozásának gyakorlásánál mennyire sikeres. Az alábbi kérdőív ebben jelenthet
+segítséget: kitöltésével tájékozódhat a képességeiről, melyek jelentik az erősségeit és melyek
+azok, amelyekben gyengébb. </p>
     </div>
   );
 }
